0e9d019f4a6ad7185ff7afcbc5911cb2331b0763
James Lansdown <jlansdown@gmail.com>
2020-04-14 17:16:11 -0700

    Added random guess testing

--- lib/gamepage.dart
+++ lib/gamepage.dart
@@ -1,28 +1,74 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 
 // ignore: must_be_immutable
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
   List<dynamic> results;
   GamePage(this.results);
+
+
+  @override
+  _GamePageState createState() => new _GamePageState();
+}
+
+
+class _GamePageState extends State<GamePage> {
+  int randomNumber;
+  String playerGuess = "";
+
+  var randomNumberGenerator = new Random();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _setText(newText) {
+    setState(() {
+      playerGuess = newText;
+    });
+  }
+
+  void _setRandomNumber(sizeOfResults) {
+    randomNumber = randomNumberGenerator.nextInt(sizeOfResults);
+  }
 
   @override
   Widget build(BuildContext context) {
 
-
     return Scaffold(
       appBar: AppBar(
-        title: Text("Second Route"),
+        title: Text("Results"),
       ),
-      body: ListView.builder(
-          itemCount: results.length,
-          itemBuilder: (context, index) {
-            return Container(
-              height: 50,
-              color: Colors.blue[50],
-              child: Center(child: Text('${results[index]['detectedClass']}' + '\n')),
-            );
-          }
+      body: Column(
+
+          children: <Widget> [
+
+            Text('$playerGuess'),
+
+            Text('Bottom'),
+
+            Expanded(
+
+              child: ListView.builder(
+                itemCount: widget.results.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${widget.results[index]['detectedClass']}'),
+                    onTap: () {
+                        _setRandomNumber(widget.results.length);
+                        _setText(
+                            '${widget.results[index]['detectedClass']}'
+                                + ' ' + '${widget.results[randomNumber]['detectedClass']}');
+                    },
+                  ); 
+                }
+            ),
+
+            ),
+            Text('Bottom'),
+      ],
       ),
     );
   }
